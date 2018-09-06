81466b78f4d6e4cd254147e8c99b410b8ae368d0
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-09-05 23:36:23 -0500

    test: Adds tests

---
+++ test.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+const mocha = require("mocha");
+
+describe("Tests", function() {
+    it("Test one", function() {
+        expect(true).to.be.true;
+    })
+});
